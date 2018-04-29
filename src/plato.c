bc4e17ad37e3fbce507b7a7c4787005cce958bdc
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2018-04-28 20:27:38 -0500

    Flesh things out a bit more. Debug dumb terminal character set selection. Serial routines still suck. Drawing too slow. ugh.

--- src/plato.c
+++ src/plato.c
@@ -52,8 +52,27 @@ unsigned long platofgcolor=0;
 unsigned long platobgcolor=0;
 unsigned int mode4start=0;
 
-const char* foo="platoterm for commodore 64";
+// PLATOTerm for Commodore 64
+const unsigned char welcomemsg_1[]={80,76,65,84,79,84,101,114,109,32,102,111,114,32,67,111,109,109,111,100,111,114,101,32,54,52};
+#define WELCOMEMSG_1_LEN 26
 
+// Copyright 2018 IRATA.ONLINE
+const unsigned char welcomemsg_2[]={67,111,112,121,114,105,103,104,116,32,40,99,41,32,0x32,0x30,0x31,0x38,0x20,73,82,65,84,65,46,79,78,76,73,78,69};
+#define WELCOMEMSG_2_LEN 31
+
+// This software is licensed under GPL 3.0.
+const unsigned char welcomemsg_3[]={84,104,105,115,32,115,111,102,116,119,97,114,101,32,105,115,32,108,105,99,101,110,115,101,100,32,117,110,100,101,114,32,71,80,76,32,51,46,48,32,46};
+#define WELCOMEMSG_3_LEN 40
+
+// See COPYING for details.
+const unsigned char welcomemsg_4[]={83,101,101,32,99,111,112,121,105,110,103,32,102,111,114,32,100,101,116,97,105,108,115};
+#define WELCOMEMSG_4_LEN 23
+
+// PLATOTerm READY
+const unsigned char welcomemsg_5[]={80,76,65,84,79,84,101,114,109,32,82,69,65,68,89};
+#define WELCOMEMSG_5_LEN 15
+
+// The static symbol for the c64 tgi graphics driver.
 extern char c64_hi;
 
 void send_byte(unsigned char b)
@@ -63,6 +82,7 @@ void send_byte(unsigned char b)
 void scroll_up(void)
 {
 }
+
 void draw_char(unsigned char charset_to_use, unsigned char char_to_plot)
 {
   a=0;
@@ -77,7 +97,7 @@ void draw_char(unsigned char charset_to_use, unsigned char char_to_plot)
   for (i=0;i<6;i++)
     {
       nx=scalex[x];
-      b=font[(a*6)+i];
+      b=font[fontptr[a]+i];
       for (j=0;j<5;j++)
 	{
 	  c=b&0x80;
@@ -110,6 +130,16 @@ void beep(void)
 {
 }
 
+void draw_string(const char* text,unsigned char len)
+{
+  unsigned char* currChar=NULL;
+  unsigned char counter=0;
+  for (counter=0;counter<len;counter++)
+    {
+      decode_dumb_terminal(text[counter]);
+    }
+}
+
 void draw_point(unsigned int x,unsigned int y)
 {
   tgi_setpixel(scalex[x],scaley[y]);
@@ -125,6 +155,17 @@ void enable_touch(int n)
 {
 }
 
+void greeting(void)
+{
+  x=168; y=480; draw_string(welcomemsg_1,WELCOMEMSG_1_LEN);
+  x=144; y=464; draw_string(welcomemsg_2,WELCOMEMSG_2_LEN);
+  x=104; y=432; draw_string(welcomemsg_3,WELCOMEMSG_3_LEN);
+  x=160; y=416; draw_string(welcomemsg_4,WELCOMEMSG_4_LEN);
+  x=16;  y=384; draw_string(welcomemsg_5,WELCOMEMSG_5_LEN);
+
+  x=0;   y=352;
+}
+
 void main(void)
 {
   int i=0;
@@ -135,9 +176,9 @@ void main(void)
   tgi_install(&c64_hi);
   tgi_init();
   tgi_clear();
-  chstr[0]='\0';
-  chstr[1]='\0';
 
+  greeting();
+  
   // set up rs-232 buffers
   *RIBUF = (char*)(((int)rs232_read_buf & 0xff00) + 256);
   *ROBUF = (char*)(((int)rs232_write_buf & 0xff00) + 256);
@@ -174,7 +215,3 @@ void main(void)
   tgi_uninstall();
 }
 
-/* void main(void) */
-/* { */
-  
-/* } */
