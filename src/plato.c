e450e2becc7cd8e97240cbd6c9cf3f93e2b40697
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2018-05-10 11:08:26 -0500

    swap nmi clear.

--- src/plato.c
+++ src/plato.c
@@ -435,8 +435,8 @@ void main(void)
   dumb_terminal_active=1;
   tgi_install(tgi_static_stddrv);
   tgi_init();
-  install_nmi_tgi();
   tgi_clear();
+  install_nmi_tgi();
   
   c=ser_open(&params);
   ser_ioctl(1, NULL);  
