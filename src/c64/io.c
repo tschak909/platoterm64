a9b688e3ebd1c14686ea16e57f35eec4a671fdd3
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2018-07-11 22:15:42 -0500

    Remove border changes for now.

--- src/c64/io.c
+++ src/c64/io.c
@@ -28,7 +28,6 @@ uint8_t io_serial_buffer_size(void)
 void io_recv_serial_flow_off(void)
 {
   // for now, assume user port.
-  POKE(0xD020,0);
   POKE(0xDD01,1);
 }
 
@@ -38,6 +37,5 @@ void io_recv_serial_flow_off(void)
 void io_recv_serial_flow_on(void)
 {
   // For now, assume user port.
-  POKE(0xD020,14);
   POKE(0xDD01,0);
 }
