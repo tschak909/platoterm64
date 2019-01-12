9578b5686dd0327e6d79b2d4852afe20cad3c590
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-01-12 02:26:36 -0600

    fix bold text.

--- src/atari/terminal_char_load.c
+++ src/atari/terminal_char_load.c
@@ -15,10 +15,12 @@
 #include "../protocol.h"
 
 /* #define FONTPTR(a) (((a << 1) + a) << 1) */
-#define FONTPTR(a) (a*6)
+#define FONTPTR(aa) (aa*6)
 
 static unsigned char char_data[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                   0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+
+static unsigned char shrunk_char_data[]={0x00,0x00,0x00,0x00,0x00,0x00};
 
 const unsigned char BTAB[]={0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01}; // flip one bit on (OR)
 const unsigned char BTAB_5[]={0x08,0x10,0x10,0x20,0x20,0x40,0x80,0x80}; // flip one bit on for the 5x6 matrix (OR)
@@ -43,10 +45,26 @@ static unsigned char PIX_WEIGHTS[]={0x00,0x00,0x00,0x00,0x00, // Pixel weights
 
 static unsigned char pix_cnt;
 static unsigned char curr_word;
-static unsigned char u,v,w;
-static unsigned char temp_byte;
+static unsigned char bb,cc,dd;
+static unsigned char algo_switch;
+static unsigned char flip_bits;
 
 extern unsigned char fontm23[768];
+
+/**
+ * Algorithm A - Approximately 54 to 85 pixels set, apply box filter
+ */
+void terminal_char_load_algo_a(void)
+{
+}
+
+/**
+ * Algorithm B - Either less than 53 pixels, or more than 84 pixels set, use a simple
+ * scaling table.
+ */
+void terminal_char_load_algo_b(unsigned char flip_bits)
+{
+}
 
 /**
  * terminal_char_load - Store a character into the user definable
@@ -55,63 +73,60 @@ extern unsigned char fontm23[768];
 void terminal_char_load(padWord charnum, charData theChar)
 {
   // Clear char data. 
-  memset(char_data,0,sizeof(char_data));
+  memset(char_data,0x00,sizeof(char_data));
   memset(PIX_WEIGHTS,0,sizeof(PIX_WEIGHTS));
   memset(&fontm23[FONTPTR(charnum)],0,6);
-
+  pix_cnt=0;
+  
   // Transpose character data.  
   for (curr_word=0;curr_word<8;curr_word++)
     {
-      for (u=16; u-->0; )
+      for (bb=16; bb-->0; )
 	{
-	  if (theChar[curr_word] & 1<<u)
+	  if (theChar[curr_word] & 1<<bb)
 	    {
 	      pix_cnt++;
-	      PIX_WEIGHTS[TAB_0_25[TAB_0_5[u]]+TAB_0_4[curr_word]]++;
-	      char_data[u^0x0f]|=BTAB[curr_word];
+	      PIX_WEIGHTS[TAB_0_25[TAB_0_5[bb]]+TAB_0_4[curr_word]]++;
+	      char_data[bb^0x0f]|=BTAB[curr_word];
 	    }
 	}
     }
   
   if ((54 <= pix_cnt) && (pix_cnt < 85))
     {
-      // Algorithm A - approx Half of pixels are set
-      for (v=6; v-->0; )
-        {
-          for (w=5; w-->0; )
-            {
-              if (PIX_WEIGHTS[TAB_0_25i[v]+w] >= PIX_THRESH[TAB_0_25i[v]+w])
-                fontm23[FONTPTR(charnum)+v]|=BTAB[w];
-            }
-        }
+      for (bb=6; bb-->0; )
+	{
+	  for (cc=5; cc-->0; )
+	    {
+	      if (PIX_WEIGHTS[TAB_0_25i[bb]+cc] >= PIX_THRESH[TAB_0_25i[bb]+cc])
+		fontm23[FONTPTR(charnum)+bb]|=BTAB[cc];
+	    }
+	}  
     }
-  else
-    {
-      // Algorithm B - Sparsely or heavily populated bitmaps
-      for (v=16; v-->0; )
-        {
-	  if (pix_cnt >= 85)
-	    char_data[v]^=0xff;
-	  
-          temp_byte = 0x00;    
-
-          for (w=8; w-->0; )
-            {
-              if (char_data[v] & (1<<w))
-                {
-                  temp_byte|=BTAB_5[w];
-                }
-	      fontm23[FONTPTR(charnum)+TAB_0_5[v]]|=temp_byte;
-            }
-        }
-
-      // Flip the bits back
-      if (pix_cnt >= 85)
-      	{
-	  for (v=6; v-->0; )
-	    fontm23[FONTPTR(charnum)+v]^=0xFF&0xF8;
-	}
-    }
-
-  // Copy shrunk char data into target charset
+    else if ((pix_cnt < 54) || (pix_cnt >= 85))
+      {
+	for (bb=16; bb-->0; )
+	  {
+	    // If dense, flip bits around.
+	    if (pix_cnt>=85)
+	      char_data[bb]^=0xff;
+	    
+	    for (cc=8; cc-->0; )
+	      {
+		if (char_data[bb] & (1<<cc))
+		  fontm23[FONTPTR(charnum)+TAB_0_5[bb]]|=BTAB_5[cc];
+	      }
+	  }
+	
+	// Flip the bits back if densely packed.
+	if (pix_cnt>=85)
+	  {
+	    for (bb=6; bb-->0; )
+	      {
+		fontm23[FONTPTR(charnum)+bb]^=0xFF;
+		fontm23[FONTPTR(charnum)+bb]&=0xF8;
+	      }
+	  }
+      }
 }
+
